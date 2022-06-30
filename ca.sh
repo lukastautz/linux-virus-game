60c249f37f90b74ee3e68b4b23375514229eb55d
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-06-30 17:04:48 +0200

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -1,4 +1,4 @@
 key=cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1
-sudo echo $key >> /etc/ca/ca-private-password.txt
-sudo openssl genrsa -des3 -out /etc/ca/ca-private.key 2048
-sudo openssl req -x509 -new -nodes -key /etc/ca/ca-private.key -sha512 -days 36500 -out /etc/ca/ca-private.pem -passout pass:"$key"
+sudo echo $key >> /var/ca/ca-private-password.txt
+sudo openssl genrsa -des3 -out /var/ca/ca-private.key 2048
+sudo openssl req -x509 -new -nodes -key /var/ca/ca-private.key -sha512 -days 36500 -out /var/ca/ca-private.pem -passout pass:"$key"
